9a0840bd614d0260784566058d99288f37cfed0b
uanetmika <78543199+uanetmika@users.noreply.github.com>
2021-10-03 22:05:18 +0700

    Add files via upload

---
+++ codecode.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp63
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("เป็นแฟนกันนะ");
+        }
+    }
+}
